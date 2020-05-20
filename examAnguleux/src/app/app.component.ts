415229ff2aec7887dd1e07f91bc7b5eec56c9f06
TofuPlatinum <quentin.legret@etu.univ-orleans.fr>
2020-05-20 15:56:04 +0200

    chrono

--- examAnguleux/src/app/app.component.ts
+++ examAnguleux/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Timer } from './timer';
+import { State } from './state';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'examAnguleux';
-}
+  title = 'app works!';
+  public _btnPlay: string = 'Start';
+  public _timer: Timer = new Timer();
+  public _state: State = new State();
+  play(){
+    this._timer.start();
+    this._state.setPlay();
+    this._btnPlay = ">";
+  }
+  stop(){
+    this._timer.stop();
+    this._state.setStop();
+  }
+  backward(){
+    this._timer.reset();
+    this._state.setBackward();
+    this._btnPlay = 'Start';
+  }
+  }
