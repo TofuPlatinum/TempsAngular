1a24b2eb5c724114cd6e0c7f838e1058493ccc1b
Fakehex <fullgcdhacked@outlook.fr>
2020-05-23 14:37:36 +0200

    route et passage de parametre

--- examAnguleux/src/app/model/tache.ts
+++ examAnguleux/src/app/model/tache.ts
@@ -1,4 +1,4 @@
-export class Projet {
+export class Tache {
   id: number;
   titre: string;
 }
--- examAnguleux/src/app/projet/projet.component.ts
+++ examAnguleux/src/app/projet/projet.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjetService } from '../services/projet-service';
-
+import { Projet } from '../model/projet';
+import { StockageLocalService } from '../services/stockage-local.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-projet',
   templateUrl: './projet.component.html',
   styleUrls: ['./projet.component.scss']
 })
 export class ProjetComponent implements OnInit {
-  id: number;
-  titre: string;
-  tache : Tache[];
+  projet: Projet;
 
-  constructor(private projetService: ProjetService) { }
+  constructor(private stockageLocalService : StockageLocalService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id: number = this.route.snapshot.params['id'];
+    let projets: Projet[] = this.stockageLocalService.recupererProjets();
+    let found: boolean = false;
+    for(let elem of projets){
+      if(elem.id == id){
+        this.projet = elem;
+        found = true;
+      }
+    }
   }
 
 }
